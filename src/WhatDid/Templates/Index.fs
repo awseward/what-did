f1566b0fb97f2fe0c2970f6f3a449ec4de04ab71
Andrew Seward <awswrd@gmail.com>
2018-11-15 01:17:41 -0800

    wip: Some more ideating
    Get rid of inputs, add some example placeholder text

--- src/WhatDid/Templates/Index.fs
+++ src/WhatDid/Templates/Index.fs
@@ -1,16 +1,32 @@
 module Index
 
 open Giraffe.GiraffeViewEngine
+open System
 
-let index = [
-  span [_class "static"] [ rawText "https://github.com/compare/"]
-  input [_placeholder "owner"; _type "text"]
-  span [_class "static"] [rawText "/"]
-  input [_placeholder "repository"; _type "text"]
-  span [_class "static"] [rawText "/compare/"]
-  input [_placeholder "base"; _type "text"]
-  span [_class "static"] [rawText "..."]
-  input [_placeholder "master"; _type "text"]
+let private exampleNotes =
+  @"
+feat:     Feature for person        https://github.com/owner/repository/pull/12
+fix:      Some dumb bug             https://github.com/owner/repository/pull/21
+fix:      Another bug               https://github.com/owner/repository/pull/18
+fix:      So many bugs              https://github.com/owner/repository/pull/30
+refactor: Rewrite in $LATEST_THING  https://github.com/owner/repository/pull/8
+style:    Make it look alright      https://github.com/owner/repository/pull/20"
+    .Trim()
+
+let index' = [
+  pre [] [
+    span [_class "static"] [ rawText "https://github.com/compare/"]
+    span [_class "dynamic"] [rawText "owner"]
+    span [_class "static"] [rawText "/"]
+    span [_class "dynamic"] [rawText "repository"]
+    span [_class "static"] [rawText "/compare/"]
+    span [_class "dynamic"] [rawText "base"]
+    span [_class "static"] [rawText "..."]
+    span [_class "dynamic"] [rawText "master"]
+    br []
+    br []
+    rawText exampleNotes
+  ]
 ]
 
-let layout = App.layout index
+let layout = App.layout index'
