c0111ba84a6b392783fdc38ce380e0fc1452895b
Andrew Seward <awswrd@gmail.com>
2018-11-16 11:02:15 -0800

    misc: Pull indentation back a little bit

--- src/WhatDid/Program.fs
+++ src/WhatDid/Program.fs
@@ -8,31 +8,33 @@ open System
 
 DotEnv.Config (
   throwOnError = false,
-  filePath = "../../.env")
+  filePath = "../../.env"
+)
+
 let config = Config.getConfig ()
 
 let isProduction = (config |> Config.isProduction)
 
 let endpointPipe = pipeline {
-    plug head
-    plug requestId
+  plug head
+  plug requestId
 }
 
 let app = application {
-    pipe_through endpointPipe
+  pipe_through endpointPipe
 
-    error_handler (fun ex _ -> pipeline { set_status_code 500; render_html (InternalError.layout isProduction ex) })
-    use_router Router.appRouter
-    url (sprintf "http://0.0.0.0:%d/" config.port)
-    memory_cache
-    use_static "static"
-    use_gzip
-    use_config (fun _ -> config)
-    use_turbolinks
+  error_handler (fun ex _ -> pipeline { set_status_code 500; render_html (InternalError.layout isProduction ex) })
+  use_router Router.appRouter
+  url (sprintf "http://0.0.0.0:%d/" config.port)
+  memory_cache
+  use_static "static"
+  use_gzip
+  use_config (fun _ -> config)
+  use_turbolinks
 }
 
 [<EntryPoint>]
 let main _ =
-    printfn "Working directory - %s" (System.IO.Directory.GetCurrentDirectory())
-    run app
-    0
+  printfn "Working directory - %s" (System.IO.Directory.GetCurrentDirectory())
+  run app
+  0
