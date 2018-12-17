8312a9603aca26e82c7d7af7ed47fc98eee93b6a
Andrew Seward <awswrd@gmail.com>
2018-12-16 17:16:15 -0800

    chore: Remove example code snippet

--- src/WhatDid/Program.fs
+++ src/WhatDid/Program.fs
@@ -14,35 +14,7 @@ DotEnv.Config (
 
 let config = Config.getConfig ()
 
-let redis =
-  let uri = Uri config.redisUrl
-  let authority = uri.Authority
-  let configStr =
-    match uri.UserInfo.Split ([|':'|]) with
-    | [|user; pass|] -> sprintf "%s,name=%s,password=%s" authority user pass
-    | _ -> authority
-  ConnectionMultiplexer.Connect configStr
-
 let isProduction = (config |> Config.isProduction)
-
-let tryRedisThings : HttpHandler = (fun next ctx ->
-  try
-    let db = redis.GetDatabase ()
-    let hashKey = RedisKey.op_Implicit "https://api.github.com/:org/:repo/compare/:base...:head"
-    let hashValue = [|
-      new HashEntry (RedisValue.op_Implicit "lastModified", RedisValue.op_Implicit (DateTimeOffset.UtcNow.ToString "o"))
-      new HashEntry (RedisValue.op_Implicit "nextUri", RedisValue.op_Implicit (string null))
-      new HashEntry (RedisValue.op_Implicit "json", RedisValue.op_Implicit "{\"foo\":true}")
-    |]
-    db.HashSet (hashKey, hashValue)
-
-    printfn "!!! hashKey !!! %A" hashKey
-    printfn "!!! hashValue' !!! %A" (db.HashGetAll hashKey)
-  with
-  | e -> printfn "ERROR: %A" e
-
-  next ctx
-)
 
 let endpointPipe = pipeline {
   plug head
@@ -51,7 +23,6 @@ let endpointPipe = pipeline {
 
 let app = application {
   pipe_through endpointPipe
-  pipe_through (pipeline { plug tryRedisThings })
 
   error_handler (fun ex _ -> pipeline { set_status_code 500; render_html (InternalError.layout isProduction ex) })
   use_router Router.appRouter
